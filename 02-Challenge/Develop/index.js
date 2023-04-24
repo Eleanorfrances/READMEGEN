84bd392e113134d7e7c4a6b39b547603877a284c
Eleanor Frances <elfrancy2002@gmail.com>
2023-04-24 08:50:25 -0600

    added question array

--- 02-Challenge/Develop/index.js
+++ 02-Challenge/Develop/index.js
@@ -1,7 +1,25 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./generateMarkdown');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+ {
+  type: 'input'
+  name: 'title'
+  message: 'What is the title of your project?'
+ },
+ {
+    type: 'input'
+    name: 'description'
+    message: 'Provide a brief description of your project!'
+ },
+ {
+   type: 'input'
+   name: 'installation'
+   message
+ }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
